f5f842503fb0ca05b5eea923efd5a1c6316e03b2
pkolin <piotrek.kolinski@gmail.com>
2017-06-27 18:12:15 +0200

    Added first file in cpp

---
+++ pkolin4/HelloWorld.cpp
@@ -0,0 +1,14 @@
+//HelloWorld.cpp: Displays "Hello World" sign
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	using::std::cout;
+	using::std::cin;
+	
+	cout << "Hello World!!!"
+	
+	return EXIT_SUCCESS;
+}
